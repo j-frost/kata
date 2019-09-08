2ade4066f002ad4ad36dcc3d9bc7e57b78bff5f8
Julian Felix Rost <j-frost+kata@a3re.net>
2019-09-08 04:57:19 +0200

    Add ruby skeleton

---
+++ lib/kata.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'kata/version'
+
+module Kata
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/kata_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Kata do
+  it 'has a version number' do
+    expect(Kata::VERSION).not_to be nil
+  end
+end
