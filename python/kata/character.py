588ce58b9ae691094615c0dde1746d5347950474
Julian Felix Rost <j-frost@a3re.net>
2020-03-02 14:30:37 +0100

    Continue work on mob programming rpg combat kata via BDD

--- python/kata/character.py
+++ python/kata/character.py
@@ -1,5 +1,14 @@
+from __future__ import annotations
+
+
 class Character(object):
 
     def __init__(self):
         self.health = 1000
         self.level = 1
+
+    def is_alive(self) -> bool:
+        return True
+
+    def attack(self, receiver: Character) -> None:
+        pass
--- python/tests/character_test.py
+++ python/tests/character_test.py
@@ -10,7 +10,7 @@ class TestCharacter(unittest.TestCase):
 
     def test_character_has_health(self):
         character = Character()
-        self.assertTrue(getattr(character, 'health'))
+        self.assertTrue(hasattr(character, 'health'), 'character must have attribute "health"')
 
     def test_new_character_has_starting_health(self):
         character = Character()
@@ -18,9 +18,28 @@ class TestCharacter(unittest.TestCase):
 
     def test_character_has_level(self):
         character = Character()
-        self.assertTrue(getattr(character, 'level'))
+        self.assertTrue(hasattr(character, 'level'), 'character must have attribute "level"')
 
     def test_new_character_has_starting_level(self):
         character = Character()
-        self.assertTrue(type(character.level) == int)
+        self.assertTrue(type(character.level) == int, 'character level must be an integer')
         self.assertEqual(character.level, 1)
+
+    def test_character_has_function_is_alive(self):
+        character = Character()
+        self.assertTrue(hasattr(character, 'is_alive'), 'character must have attribute "is_alive"')
+        self.assertTrue(callable(character.is_alive), 'character "is_alive" attribute must be callable')
+
+    def test_new_character_is_alive(self):
+        character = Character()
+        self.assertTrue(character.is_alive())
+
+    def test_character_has_function_attack(self):
+        character = Character()
+        self.assertTrue(hasattr(character, 'attack'), 'character must have attribute "attack"')
+        self.assertTrue(callable(character.attack), 'character "attack" attribute must be callable')
+
+    def test_character_can_attack_another_character(self):
+        attacker = Character()
+        receiver = Character()
+        attacker.attack(receiver)
